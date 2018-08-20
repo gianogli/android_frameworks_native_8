a79513155816eea2ffab8dce2c1f3e3b5c0f3e87
Shilin Victor <chrono.monochrome@gmail.com>
2018-08-20 22:57:43 +0300

    Remove -Werror

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -105,7 +105,7 @@ LOCAL_EXPORT_SHARED_LIBRARY_HEADERS := \
     libhidltransport \
     libhwbinder
 
-LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code -std=c++1z
+LOCAL_CFLAGS += -Wall -Wunused -Wunreachable-code -std=c++1z
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -154,7 +154,7 @@ ifdef TARGET_32_BIT_SURFACEFLINGER
 LOCAL_32_BIT_ONLY := true
 endif
 
-LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
+LOCAL_CFLAGS += -Wall -Wunused -Wunreachable-code
 
 include $(BUILD_EXECUTABLE)
 
@@ -177,7 +177,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_MODULE := libsurfaceflinger_ddmconnection
 
-LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
+LOCAL_CFLAGS += -Wall -Wunused -Wunreachable-code
 
 include $(BUILD_SHARED_LIBRARY)
 endif # libnativehelper
