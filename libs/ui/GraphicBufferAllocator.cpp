16c3eb14065f9b5dc254cecf5ab705c3074b3387
gianogli <gianogli@tiscali.it>
2019-02-04 01:38:40 +0100

    Revert "ui: Allocator: exynos4 hacks"
    This reverts commit d0f9edf14010b9071662a55fac19b6d68c4312d0.

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -18,8 +18,6 @@
 #define LOG_TAG "GraphicBufferAllocator"
 #define ATRACE_TAG ATRACE_TAG_GRAPHICS
 
-#include <hardware/gralloc.h>
-
 #include <ui/GraphicBufferAllocator.h>
 
 #include <stdio.h>
@@ -39,28 +37,18 @@ namespace android {
 
 ANDROID_SINGLETON_STATIC_INSTANCE( GraphicBufferAllocator )
 
-static alloc_device_t  *mAllocDev;
 Mutex GraphicBufferAllocator::sLock;
 KeyedVector<buffer_handle_t,
     GraphicBufferAllocator::alloc_rec_t> GraphicBufferAllocator::sAllocList;
 
 GraphicBufferAllocator::GraphicBufferAllocator()
-    : mMapper(GraphicBufferMapper::getInstance()),
-      mAllocator(std::make_unique<Gralloc2::Allocator>(
-                 mMapper.getGrallocMapper()))
+  : mMapper(GraphicBufferMapper::getInstance()),
+    mAllocator(std::make_unique<Gralloc2::Allocator>(
+                mMapper.getGrallocMapper()))
 {
-    hw_module_t const* module;
-    int err = hw_get_module(GRALLOC_HARDWARE_MODULE_ID, &module);
-    ALOGE_IF(err, "FATAL: can't find the %s module", GRALLOC_HARDWARE_MODULE_ID);
-    if (err == 0) {
-        gralloc_open(module, &mAllocDev);
-    }
 }
 
-GraphicBufferAllocator::~GraphicBufferAllocator()
-{
-    gralloc_close(mAllocDev);
-}
+GraphicBufferAllocator::~GraphicBufferAllocator() {}
 
 void GraphicBufferAllocator::dump(String8& result) const
 {
@@ -116,22 +104,19 @@ status_t GraphicBufferAllocator::allocate(uint32_t width, uint32_t height,
     if (!width || !height)
         width = height = 1;
 
-    // we have a h/w allocator and h/w buffer is requested
-    status_t err;
+    // Ensure that layerCount is valid.
+    if (layerCount < 1)
+        layerCount = 1;
 
-    // Filter out any usage bits that should not be passed to the gralloc module
-    usage &= GRALLOC_USAGE_ALLOC_MASK;
+    Gralloc2::IMapper::BufferDescriptorInfo info = {};
+    info.width = width;
+    info.height = height;
+    info.layerCount = layerCount;
+    info.format = static_cast<Gralloc2::PixelFormat>(format);
+    info.usage = usage;
 
-    int outStride = 0;
-    err = mAllocDev->alloc(mAllocDev, static_cast<int>(width),
-            static_cast<int>(height), format, static_cast<int>(usage), handle,
-            &outStride);
-    *stride = static_cast<uint32_t>(outStride);
-
-    ALOGW_IF(err, "alloc(%u, %u, %d, %llx, ...) failed %d (%s)",
-            width, height, format, usage, err, strerror(-err));
-
-    if (err == NO_ERROR) {
+    Gralloc2::Error error = mAllocator->allocate(info, stride, handle);
+    if (error == Gralloc2::Error::NONE) {
         Mutex::Autolock _l(sLock);
         KeyedVector<buffer_handle_t, alloc_rec_t>& list(sAllocList);
         uint32_t bpp = bytesPerPixel(format);
@@ -145,34 +130,30 @@ status_t GraphicBufferAllocator::allocate(uint32_t width, uint32_t height,
         rec.size = static_cast<size_t>(height * (*stride) * bpp);
         rec.requestorName = std::move(requestorName);
         list.add(*handle, rec);
-    }
 
-    return err;
+        return NO_ERROR;
+    } else {
+        ALOGE("Failed to allocate (%u x %u) layerCount %u format %d "
+                "usage %" PRIx64 ": %d",
+                width, height, layerCount, format, usage,
+                error);
+        return NO_MEMORY;
+    }
 }
 
 status_t GraphicBufferAllocator::free(buffer_handle_t handle)
 {
     ATRACE_CALL();
-    status_t err;
-
-    if (handle == NULL) {
-        ALOGE("%s: not attempting to free null pointer handle", __func__);
-        return NO_ERROR;
-    }
 
     // We allocated a buffer from the allocator and imported it into the
     // mapper to get the handle.  We just need to free the handle now.
-    //mMapper.freeBuffer(handle);
-    err = mAllocDev->free(mAllocDev, handle);
+    mMapper.freeBuffer(handle);
 
-    ALOGW_IF(err, "free(...) failed %d (%s)", err, strerror(-err));
-    if (err == NO_ERROR) {
-        Mutex::Autolock _l(sLock);
-        KeyedVector<buffer_handle_t, alloc_rec_t>& list(sAllocList);
-        list.removeItem(handle);
-    }
+    Mutex::Autolock _l(sLock);
+    KeyedVector<buffer_handle_t, alloc_rec_t>& list(sAllocList);
+    list.removeItem(handle);
 
-    return err;
+    return NO_ERROR;
 }
 
 // ---------------------------------------------------------------------------
